e790b46998b24cb5903a97b3422a3fba8dbd90cd
onumoh abubakar <1abubakaronumoh@gmail.com>
2022-05-22 14:00:19 +0100

    All-pages-fixed
    Event listeners can be added to each button without getting a null error.Also, font-sizes for sign-up-option as been fixed to enable proper responsiveness and the url for each pages have been fixed to append and remove on click.

--- src/Js/loader.js
+++
@@ -1,9 +0,0 @@
-const preloader = () => {
-  const loader = document.getElementById("preloader");
-
-  window.addEventListener("load", () => {
-    loader.style.display = "none";
-  });
-};
-
-export { preloader as default };
